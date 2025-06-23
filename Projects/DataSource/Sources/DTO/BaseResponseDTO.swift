408961ef510e363e8820173588b72a6f4c1b7108
최정인 <wjddls980912@naver.com>
2025-06-23 17:34:46 +0900

    Feat: BaseResponseDTO 구현 (#T3-57)

---
+++ Projects/DataSource/Sources/DTO/BaseResponseDTO.swift
@@ -0,0 +1,12 @@
+//
+//  BaseResponseDTO.swift
+//  DataSource
+//
+//  Created by 최정인 on 6/23/25.
+//
+
+struct BaseResponseDTO<T: Decodable>: Decodable {
+    let code: String
+    let data: T
+    let message: String
+}
