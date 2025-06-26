085e67ac7ec2d07a19e4cb4ecc6293840771a36c
최정인 <wjddls980912@naver.com>
2025-06-26 13:29:31 +0900

    Feat: ViewModel protocol 구현 (#T3-65)

---
+++ Projects/Presentation/Sources/Protocols/ViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  ViewModel.swift
+//  Presentation
+//
+//  Created by 최정인 on 6/26/25.
+//
+
+import Foundation
+
+public protocol ViewModel {
+    associatedtype Input
+    associatedtype Output
+
+    func action(input: Input)
+}
