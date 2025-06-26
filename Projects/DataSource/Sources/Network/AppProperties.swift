5e92d10617795ef5b92ae50f26d7bac01a25e2cf
최정인 <wjddls980912@naver.com>
2025-06-26 17:26:04 +0900

    Chore: Project 파일 수정
    - 각 모듈의 infoPlist 설정 제거
    - App 단의 info.plist 파일 사용하도록 수정

--- Projects/DataSource/Sources/Network/AppProperties.swift
+++ Projects/DataSource/Sources/Network/AppProperties.swift
@@ -9,10 +9,6 @@ import Foundation
 
 enum AppProperties {
     static var baseURL: String {
-        Bundle.module.object(forInfoDictionaryKey: "BaseURL") as? String ?? ""
-    }
-
-    static var test: String {
-        Bundle.module.object(forInfoDictionaryKey: "CFBundleName") as? String ?? "네임 읽기 실패 ...."
+        Bundle.main.object(forInfoDictionaryKey: "BaseURL") as? String ?? ""
     }
 }
