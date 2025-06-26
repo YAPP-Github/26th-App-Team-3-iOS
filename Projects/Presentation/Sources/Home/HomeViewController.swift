b60369e3af3b08bf8d13378e86471e00a0b0b42d
최정인 <wjddls980912@naver.com>
2025-06-26 22:31:01 +0900

    Refactor: HomeViewController에 디자인 시스템 적용

--- Projects/Presentation/Sources/Home/HomeViewController.swift
+++ Projects/Presentation/Sources/Home/HomeViewController.swift
@@ -21,11 +21,11 @@ public final class HomeViewController: BaseViewController<HomeViewModel> {
         cancellables = []
         super.init(viewModel: viewModel)
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     public override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -34,9 +34,8 @@ public final class HomeViewController: BaseViewController<HomeViewModel> {
         view.backgroundColor = .blue
 
         label.do {
-            $0.textColor = .white
-            $0.font = .systemFont(ofSize: 20, weight: .semibold)
-            $0.textAlignment = .center
+            $0.textColor = BitnagilColor.gray90
+            $0.attributedText = BitnagilFont.headline1.attributedString(text: nil)
         }
 
         viewModel.action(input: .healthCheck)
