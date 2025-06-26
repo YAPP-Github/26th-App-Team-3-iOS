338621dd994c3a1ae8626803d59e85c5f9535231
최정인 <wjddls980912@naver.com>
2025-06-27 00:32:09 +0900

    Feat: DependencyAssemblerProtocol 구현 (#T3-63)

---
+++ Projects/Shared/Sources/DIContainer/DependencyAssemblerProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  DependencyAssemblerProtocol.swift
+//  Shared
+//
+//  Created by 최정인 on 6/26/25.
+//
+
+import Foundation
+
+public protocol DependencyAssemblerProtocol {
+    func assemble()
+}
