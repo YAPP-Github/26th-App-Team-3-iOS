ccac622ae20bf37b19b86fd1c6f1d9fa9bffd100
최정인 <wjddls980912@naver.com>
2025-07-02 14:52:57 +0900

    Feat: AuthRepository appleLogin 구현 (#T3-67)
    - AuthRepositoryProtocol, AuthRepository appleLogin 정의 및 구현
    - 받은 닉네임, 토큰 값을 통해 서버 통신 로직 구현
    - UserDefaults에 닉네임 저장 및 불러오는 로직 구현

--- Projects/DataSource/Sources/Repositories/AuthRepository.swift
+++ Projects/DataSource/Sources/Repositories/AuthRepository.swift
@@ -14,15 +14,32 @@ import KakaoSDKAuth
 public final class AuthRepository: AuthRepositoryProtocol {
     private let networkService: NetworkServiceProtocol
     private let keychainStorage: KeychainStorageProtocol
+    private let userDefaultsStorage: UserDefaultsStorageProtocol
 
-    public init(networkService: NetworkServiceProtocol, keychainStorage: KeychainStorageProtocol) {
+    public init(
+        networkService: NetworkServiceProtocol,
+        keychainStorage: KeychainStorageProtocol,
+        userDefaultsStorage: UserDefaultsStorageProtocol
+    ) {
         self.networkService = networkService
         self.keychainStorage = keychainStorage
+        self.userDefaultsStorage = userDefaultsStorage
     }
 
     public func kakaoLogin() async throws {
         let accessToken = try await fetchKakaoToken()
         try await requestServerLogin(socialType: .kakao, nickname: nil, token: accessToken)
+    }
+
+    public func appleLogin(nickname: String?, authToken: String) async throws {
+        var savedNickname: String = ""
+        if let nickname {
+            try self.saveNickname(nickname: nickname)
+            savedNickname = nickname
+        } else {
+            savedNickname = try self.loadNickname()
+        }
+        try await requestServerLogin(socialType: .apple, nickname: savedNickname, token: authToken)
     }
 
     private func fetchKakaoToken() async throws -> String {
@@ -62,7 +79,7 @@ public final class AuthRepository: AuthRepositoryProtocol {
         guard saveToken(tokenType: .accessToken, token: tokenEntity.accessToken),
               saveToken(tokenType: .refreshToken, token: tokenEntity.refreshToken)
         else {
-            throw AuthError.failTokenSave
+            throw AuthError.tokenSaveFailed
         }
         BitnagilLogger.log(logType: .debug, message: "AccessToken Saved: \(tokenEntity.accessToken)")
         BitnagilLogger.log(logType: .debug, message: "RefreshToken Saved: \(tokenEntity.refreshToken)")
@@ -70,5 +87,19 @@ public final class AuthRepository: AuthRepositoryProtocol {
 
     private func saveToken(tokenType: TokenType, token: String) -> Bool {
         return keychainStorage.save(token, forKey: tokenType.rawValue)
+    }
+
+    private func loadNickname() throws -> String {
+        let nickname: String? = userDefaultsStorage.load(forKey: UserDefaultsKey.nickname.rawValue)
+        guard let nickname else {
+            throw AuthError.nicknameLoadFailed
+        }
+        return nickname
+    }
+
+    private func saveNickname(nickname: String) throws {
+        guard userDefaultsStorage.save(nickname, forKey: UserDefaultsKey.nickname.rawValue) else {
+            throw AuthError.nicknameSaveFailed
+        }
     }
 }
