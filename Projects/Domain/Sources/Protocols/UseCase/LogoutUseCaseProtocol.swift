cb9664011aa00a60ea7964dfefe8596511709b0c
최정인 <wjddls980912@naver.com>
2025-07-04 18:56:23 +0900

    Feat: LogoutUseCase 구현 (#T3-75)
    - LogoutUseCase Protocol, LogoutUsecase logout 함수 정의 및 구현
    - LoginUseCase 폴더 이동

---
+++ Projects/Domain/Sources/Protocols/UseCase/LogoutUseCaseProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  LogoutUseCaseProtocol.swift
+//  Domain
+//
+//  Created by 최정인 on 7/4/25.
+//
+
+import Foundation
+
+public protocol LogoutUseCaseProtocol {
+    /// 로그아웃을 진행합니다.
+    func logout() async throws
+}
